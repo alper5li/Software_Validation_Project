fc19e9d2b5c07dd0f8d608b8582a937927741927
Alper Beşli <alper5li@users.noreply.github.com>
2023-01-08 03:22:57 +0300

    sql integration left

--- SVT_Server/app.js
+++ SVT_Server/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
 
@@ -17,10 +17,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//  href="/static/javascripts/nicepage.js"
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- SVT_Server/routes/index.js
+++ SVT_Server/routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/Ana-Sayfa', function(req,res,next){
+  res.render('Ana-Sayfa.hbs', { title: 'Express' });
+})
+
+router.get('/Hakkimda', function(req,res,next){
+  res.render('Hakkimda.hbs', { title: 'Express' });
+})
+
+router.get('/Iletisim', function(req,res,next){
+  res.render('Iletisim.hbs', { title: 'Express' });
+})
+
+router.get('/login', function(req,res,next){
+  res.render('login', { title: 'Express' });
+})
+
 module.exports = router;
